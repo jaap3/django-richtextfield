f715a4bb1722048f659a3d023c051b3f3889f65c
Jaap Roes <jaap@eight.nl>
2014-09-11 14:18:42 +0200

    Widget takes wysiwyg_conf and sets djwysiwyg as css class

--- djwysiwygfield/widgets.py
+++ djwysiwygfield/widgets.py
@@ -2,4 +2,11 @@ from django.forms.widgets import Textarea
 
 
 class WysiwygWidget(Textarea):
-    pass
+    def __init__(self, attrs=None, wysiwyg_conf=None):
+        defaults = {'class': 'djwysiwyg'}
+        if attrs:
+            if 'class' in attrs:
+                attrs['class'] = ' '.join([attrs['class'], defaults['class']])
+            defaults.update(attrs)
+        self.wysiwyg_conf = wysiwyg_conf
+        super(WysiwygWidget, self).__init__(defaults)

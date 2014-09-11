110bd95e8db7930781b90686bd72b447d9410336
Jaap Roes <jaap@eight.nl>
2014-09-11 14:18:07 +0200

    Correct autodiscover call

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-admin.site.autodiscover()
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),

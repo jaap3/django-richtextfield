cf70c1bba82f821805002b7eafa910c169436f33
Jaap Roes <jaap@eight.nl>
2014-09-23 15:15:40 +0200

    Convert callback strings to functions for TinyMCE

--- djrichtextfield/templates/djrichtextfield/init/tinymce.js
+++ djrichtextfield/templates/djrichtextfield/init/tinymce.js
@@ -1,4 +1,18 @@
+function fixCallbacks(settings) {
+    /* Takes a settings object. Converts known callback keys to global
+     * window functions. */
+    var callback, callbacks = [
+        'setup', 'init_instance_callback', 'color_picker_callback',
+        'file_picker_callback', 'file_browser_callback'
+    ];
+    while (callback = callbacks.pop()) {
+        if (callback in settings && window[settings[callback]]) {
+            settings[callback] = window[settings[callback]];
+        }
+    }
+    return settings;
+}
 if (!tinymce.editors[id]) {
     settings.selector = "#" + id;
-    tinymce.init(settings);
+    tinymce.init(fixCallbacks(settings));
 }

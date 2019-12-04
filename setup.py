ac108c77e87380e140d751aaffb3e624e607cb7e
Jaap Roes <jroes@leukeleu.nl>
2019-12-04 10:03:29 +0100

    Back to development: 1.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '1.5.0'
+version = '1.5.1.dev0'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel upload')
